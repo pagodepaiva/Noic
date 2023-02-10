8739c0dd232b78ae878c7336f71fdc2a259e0c37
pagodepaiva <90423475+pagodepaiva@users.noreply.github.com>
2023-02-10 11:23:50 -0300

    Update cinema.cpp

--- cinema.cpp
+++ cinema.cpp
@@ -6,7 +6,13 @@ int main(){
     int a, b;
 	cin >> a >> b;
 	
-	int con = 0;
+	int con = 0; 
+	
+	/*
+	IMPORTANTE: Quando for iniciar um contador parecido com o do esse problema, sempre se
+	lembre de iniciar o con com o valor 0, pois, se você não fizer isso, o compilador vai
+	colocar um valor aleatório no con, dando valores absurdos pro seu problema.
+	*/
 	
 	if(a < 18) con += 15;
 	else if(a >= 60) con += 20;
