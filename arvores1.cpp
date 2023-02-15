2706ac74523ff0448dc1fa8ca1befaf6d7cc96ec
pagodepaiva <90423475+pagodepaiva@users.noreply.github.com>
2023-02-14 21:50:36 -0300

    Update arvores1.cpp

--- arvores1.cpp
+++ arvores1.cpp
@@ -1,54 +1,38 @@
 #include<bits/stdc++.h>
-#define int long long
-#define ms(v) memset(v, -1, sizeof v)
-#define pb push_back
-#define mp make_pair
-#define sv size
-#define ll long long int
-#define pi pair <int,int>
-#define itn int
-#define fr first
-#define sc second
-#define srt(v) sort(v.begin(), v.end())
-#define rvs(v) reverse(v.begin(), v.end())
 #define N 200010
- 
+#define pb push_back
+
 using namespace std;
- 
+
 int n;
 vector <int> g[N];
-int sz[N];
- 
-int solve(int v){
-    sz[v] = 0;
- 
+int dp[N];
+
+int solve(int v, int p){
     for(auto x : g[v]){
-        sz[v] += solve(x);
+        if(x == p) continue;
+        dp[v] += solve(x, v);
     }
- 
-    sz[v]++;
-    return sz[v];
+
+    dp[v]++;
+    return dp[v];
 }
- 
-main(){
-    ios::sync_with_stdio(false); cin.tie(0);
-    
-    ms(sz);
- 
+
+int main(){
     cin >> n;
- 
+
     for(int i = 2;i <= n;i++){
         int a;
         cin >> a;
         g[a].pb(i);
+        g[i].pb(a);
     }
- 
-    solve(1);
+
+    solve(1, 1);
+
     for(int i = 1;i <= n;i++){
-        cout << --sz[i] << " ";
+        cout << --dp[i] << "\n";
     }
- 
-    cout << "\n";
-    
+
     return 0;
 }
