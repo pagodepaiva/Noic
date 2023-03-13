47cd67d77d4c515fe735eadf15f1a6b4df678317
pagodepaiva <90423475+pagodepaiva@users.noreply.github.com>
2023-03-13 09:20:48 -0300

    Update torre_de_dados.cpp

--- torre_de_dados.cpp
+++ torre_de_dados.cpp
@@ -1,54 +1,81 @@
 #include<bits/stdc++.h>
+#define pb push_back
+
 using namespace std;
-int n,v[1010][7],m[1010][7],maior;
-int vinc(int x){
-    if(x == 1){
-        return 6;
-    }
-    if(x == 2){
-        return 4;
-    }
-    if(x == 3){
-        return 5;
-    }
-    if(x == 4){
-        return 2;
-    }
-    if(x == 5){
-        return 3;
-    }
-    return 1;
+
+int dadolol(int x){
+    if(x == 0) return 5;
+    if(x == 1) return 3;
+    if(x == 2) return 4;
+    if(x == 3) return 1;
+    if(x == 4) return 2;
+    if(x == 5) return 0;
 }
-int valor(int a ,int b){
-    if(a != 6 && b != 6){
-        return 6;
-    }
-    if(a != 5 && b != 5){
-        return 5;
-    }
-    return 4;
-}
+
 int main(){
+    int n;
     cin >> n;
-    for(int i = 1;i <= n;i++){
-        for(int j = 1;j <= 6;j++){
-            cin >> v[i][j];
-        }
+
+    vector < array <int, 6> > v;
+
+    for(int i = 0;i < n;i++){
+        int a, b, c, d, e, f;
+        cin >> a >> b >> c >> d >> e >> f;
+
+        v.pb({a, b, c, d, e, f});
     }
-    for(int i = 1;i <= n;i++){
-        for(int j = 1;j <= 6;j++){
-            for(int k = 1;k <= 6;k++){
-                if(v[i][k] == j){
-                    int g = vinc(k);
-                    int x = valor(v[i][g] , j);
-                    m[i][v[i][g]] = m[i - 1][j] + x;
-                }
+
+    int resmax = 0;
+
+    for(int x = 0;x < 6;x++){
+        int res = 0;
+        int ban = x;
+        int valf = v[0][x], valb = v[0][dadolol(x)];
+        int m = 0;
+
+        // cout << valf << " " << valb << "\n";
+
+
+        for(int i = 0;i < 6;i++){
+            if(v[0][i] == valf or v[0][i] == valb) continue;
+            m = max(m, v[0][i]);
+        }
+
+        res += m;
+		resmax = max(resmax, res);
+
+        if(n == 1) continue;
+
+        valf = valb;
+
+        for(int i = 0;i < 6;i++){
+            if(valf == v[1][i]) ban = i;
+        }
+
+        valb = v[1][dadolol(ban)];
+
+        for(int i = 1;i < n;i++){
+            // cout << valf << " " << valb << "\n";
+
+            m = 0;
+            for(int j = 0;j < 6;j++){
+                if(v[i][j] == valf or v[i][j] == valb) continue;
+                m = max(m, v[i][j]);
             }
+
+            res += m;
+            if(i == n-1) continue;
+            valf = valb;
+            for(int t = 0;t < 6;t++){
+                if(valf == v[i+1][t]) ban = t;
+            }
+            valb = v[i+1][dadolol(ban)];
         }
+
+        // cout << "\n" << " " << "\n";
+
+        resmax = max(resmax, res);
     }
-    for(int i = 1;i <= 6;i++){
-        maior = max(maior , m[n][i]);
-    }
-    cout << maior;
-    return 0;
+
+    cout << resmax << "\n";
 }
