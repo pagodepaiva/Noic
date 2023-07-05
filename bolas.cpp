8f2c9b4e620cb0f0ac5bcb06267c06c54656f8cf
pagodepaiva <90423475+pagodepaiva@users.noreply.github.com>
2023-07-05 08:35:17 -0300

    Update bolas.cpp

--- bolas.cpp
+++ bolas.cpp
@@ -1,38 +1,31 @@
 #include<bits/stdc++.h>
-#define int long long
-#define N 200010
+#define inf 1e8
 
 using namespace std;
 
-int dp[N][2];
-int at[N];
-
-void solve(){
-    int n;
-    cin >> n;
-    dp[0][0] = dp[0][1] = 0;
-
-    for(int i = 1;i <= n;i++){
-        int x;
-        cin >> x;
-        dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
-        dp[i][1] = (at[x] == 0 ? 0 : max(max(dp[at[x]-1][0], dp[at[x]-1][1]) + i - (at[x]-1), dp[at[x]][1] + i-at[x])); 
-        at[x] = i;
-    }
-
-    cout << max(dp[n][0], dp[n][1]) << "\n";
-
-    for(int i = 0;i <= n;i++){
-        // cout << dp[i][0] << " " << dp[i][1] << "\n";
-        dp[i][0] = dp[i][1] = at[i] = 0;
-    }
-}
-
-int32_t main(){
+int main(){
     int t;
     cin >> t;
+
     while(t--){
-        solve();
+        int n;
+        cin >> n;
+
+        int dp[n+1], m[n+1];
+        dp[0] = 0;
+
+        for(int i = 0;i < n+1;i++) m[i] = inf;
+
+        for(int i = 1;i <= n;i++){
+            int x;
+            cin >> x;
+
+            dp[i] = min(dp[i-1]+1, m[x]);
+            m[x] = min(m[x], dp[i-1]);
+        }
+
+        cout << n - dp[n] << "\n";
     }
+
     return 0;
 }
