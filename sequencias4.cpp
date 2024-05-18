c349401e93b92c65b35b4c18e4db409d4c306d1d
pagodepaiva <90423475+pagodepaiva@users.noreply.github.com>
2024-05-18 00:56:43 -0300

    Create sequencias4.cpp

---
+++ sequencias4.cpp
@@ -0,0 +1,253 @@
+#include<bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+int lg(int x){
+    int prod = 1, con = 0;
+
+    while(prod <= x){
+        if(prod*2 > x) return con;
+        con++;
+        prod *= 2;
+    }
+
+    return con;
+}
+
+vector <int> solve1(int n){
+    vector <int> fat;
+
+    for(int i = 2;i*i <= n;i++){
+        while(n % i == 0){
+            fat.push_back(i);
+            n = n/i;
+        }
+    }
+
+    if(n != 1) fat.push_back(n);
+
+    vector <int> res;
+    
+    int con = 1;
+
+    for(auto x : fat){
+        for(int i = con+x-1;i >= con;i--){
+            res.push_back(i);
+        }
+        con = con + x;
+    }
+
+    return res;
+}
+
+vector <int> solve2(int n){
+    vector <int> p = solve1(n-1);
+
+    int val = n+1, con = 0;
+    int m = p.size();
+
+    reverse(p.begin(), p.end());
+
+    for(auto x : p){
+        if(val > x){
+            val = x;
+            con++;
+        }
+    }
+
+        // reverse(p.begin(), p.end());
+    for(int i = m+con;i >= m+1;i--){
+        p.push_back(i);
+    }
+
+    reverse(p.begin(), p.end());
+
+    return p;
+}
+
+vector <int> solve3(int n){
+    vector <int> res;
+    int con = 0;
+    int qt = 0;
+
+    for(int i = 0;i <= 30;i++) if(((1 << i) & n) != 0) qt = i;
+
+    for(int i = 0;i <= 30;i++){
+        if(((1 << i) & n) == 0) continue;
+        vector <int> ans = solve1((1 << i));
+        reverse(res.begin(), res.end());
+        
+        int t = ans.size();
+        t /= 2;
+        int k = ans.size();
+
+        for(int i = k + 1; i <= k + qt - t;i++){
+            ans.push_back(i);
+        }
+
+        reverse(ans.begin(), ans.end());
+        
+        for(auto x : ans){
+            res.push_back(x+con);
+        }
+
+        reverse(res.begin(), res.end());
+
+        int aux = 0;
+
+        for(auto x : ans){
+            aux = max(aux, x);
+        }    
+
+        con = con+aux;
+    }   
+
+    // for(auto x : res) cout << x << ' ';
+    //     cout << '\n';
+    return res;
+}
+
+vector <int> solve(int n){
+    if(n == 1){
+        return {1};
+    }
+
+    if(n == 2){
+        // cout << 2 << '\n';
+        // cout << 2 << ' ' << 1 << '\n';
+        return {2,1};
+    }
+
+    vector <int> res = solve1(n);
+    vector <int> aux = solve2(n);
+
+    if(aux.size() < res.size()) res = aux;
+
+    aux = solve3(n);
+    // cout << aux.size() << '\n';
+
+    if(aux.size() < res.size()) res = aux;
+
+    return res;
+}
+
+int32_t main(){
+    int n;
+    cin >> n;
+
+    if(n == 1){
+        cout << 1 << '\n';
+        cout << 1 << '\n';
+        return 0;
+    }
+
+    if(n == 2){
+        cout << 2 << '\n';
+        cout << 2 << ' ' << 1 << '\n';
+        return 0;
+    }
+
+    vector <int> res = solve(n);
+
+    for(int i = 1;i < min(n, (int) (1e9 + 5e8)/n);i++){
+        vector <int> a1, a2;
+
+        a1 = solve(i);
+        a2 = solve(n-i);
+
+        // if(a1.size() < a2.size()) swap(a1, a2);
+
+        // cout << i << ' ';
+
+        // cout << '\n';
+
+        // for(auto x : a1){
+        //     cout << x << ' ';
+        // }
+
+        // cout << '\n';
+
+        // for(auto x : a2){
+        //     cout << x << ' ';
+        // }
+
+
+        int val = n+1, con = 0;
+
+        // cout << "p1";
+        for(int i = a1.size() -1;i >= 0;i--){
+            if(val > a1[i]){
+                val = a1[i];
+                con++;
+            }
+
+            // cout << val << ' ';
+        }   
+
+        // cout << "pq";
+
+        int con2 = 0;
+
+        val = n+1;
+
+        // cout << '\n' << "orje";
+
+        for(int i = a2.size()-1;i >= 0;i--){
+            if(val > a2[i]){
+                val = a2[i];
+                con2++;
+            }
+            // cout << val << ' ';
+        }
+
+        // cout << "oefja ";
+        if(con < con2) swap(a1, a2);
+        if(con < con2) swap(con, con2);
+
+        int t = a2.size();
+
+        for(int i = t+1;i <= t + con - con2;i++){
+            a2.push_back(i);
+        }
+
+        for(int i = 0;i < a2.size();i++){
+            int x = a2[i];
+            x = x + a1.size();
+            // cout << x << ' ';
+            a2[i] = x;
+        }
+
+
+        reverse(a2.begin(), a2.end());
+        reverse(a1.begin(), a1.end());
+
+        for(auto x : a2){
+            a1.push_back(x);
+        }
+
+        reverse(a1.begin(), a1.end());
+
+        // cout << '\n';
+        // for(auto x : a1){
+        //     cout << x << ' ';
+        // }
+
+        // cout << '\n';
+        // cout << '\n';
+
+        if(a1.size() < res.size()){
+            res = a1;
+        }
+    }
+
+    cout << res.size() << '\n';
+
+    for(auto x : res){
+        cout << x << ' ';
+    }
+
+    cout << '\n';
+
+    return 0;
+}
