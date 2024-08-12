f56cb7294aa491281b98dcfd39b6f4a3cf2d8215
pagodepaiva <90423475+pagodepaiva@users.noreply.github.com>
2024-08-12 08:31:00 -0300

    Create pedras.cpp

---
+++ pedras.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int N; cin >> N;
+
+    if(N % 3 == 0) cout << "Lalic\n";
+    if(N % 3 == 1) cout << "Lalic\n";
+    if(N % 3 == 2) cout << "Enzo\n";
+}
